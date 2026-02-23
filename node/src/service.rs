64025e4b2382dc974992d4a04a2f7be53374f99b
Davide Galassi <davxy@datawok.net>
2026-02-23 15:52:48 +0100

    Fix

--- node/src/service.rs
+++ node/src/service.rs
@@ -75,6 +75,7 @@ pub fn new_partial(config: &Configuration) -> Result<Service, ServiceError> {
             config,
             telemetry.as_ref().map(|(_, telemetry)| telemetry.handle()),
             executor,
+            Default::default(),
         )?;
     let client = Arc::new(client);
 
@@ -154,6 +155,7 @@ pub fn new_full<Network: sc_network::NetworkBackend<Block, <Block as BlockT>::Ha
             client: client.clone(),
             transaction_pool: transaction_pool.clone(),
             spawn_handle: task_manager.spawn_handle(),
+            spawn_essential_handle: task_manager.spawn_essential_handle(),
             import_queue,
             block_announce_validator_builder: None,
             warp_sync_config: None,
